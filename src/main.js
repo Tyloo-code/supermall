4501ff7196e918b629d82801e22cfb0e6b592a72
bjfu_ltl <1002705465@qq.com>
2020-11-24 00:29:22 +0800

    tabcontrol吸顶效果实现

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 Vue.config.productionTip = false
 Vue.config.devtools = true;
 
+Vue.prototype.$bus = new Vue()
+
 new Vue({
   render: h => h(App),
   router
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,38 +1,48 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    
-    <scroll class="content" ref="scroll">
-    <home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/>
-    <feature-view/>
-    <tab-control class="tab-control" 
-                 :titles="['流行', '新款', '精选']"
-                 @tabClick="tabClick"/>
-    <goods-list :goods="showGoods"/>
+    <tab-control
+        :titles="['流行', '新款', '精选']"
+        @tabClick="tabClick"
+        ref="tabControl1"
+        class="tab-control"
+        v-show="isTabFixed"/>
+    <scroll
+      class="content"
+      ref="scroll"
+      :probe-type="3"
+      @scroll="contentScroll"
+      :pull-up-load="true"
+      @pullingUp="loadMore">
+      <home-swiper :banners="banners" @swiperImageLoad="swiperImageLoad"/>
+      <recommend-view :recommends="recommends" />
+      <feature-view />
+      <tab-control
+        :titles="['流行', '新款', '精选']"
+        @tabClick="tabClick"
+        ref="tabControl2"/>
+      <goods-list :goods="showGoods" />
     </scroll>
-    <back-top @click.native="backClick"/>
-    
+    <back-top @click.native="backClick" v-show="isShowBackTop" />
   </div>
 </template>
 
 <script>
-import HomeSwiper from './childComps/HomeSwiper';
-import RecommendView from './childComps/RecommendView'
-import FeatureView from './childComps/FeatureView'
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 
-import NavBar from 'components/common/navbar/NavBar';
-import TabControl from 'components/content/tabControl/TabControl';
-import GoodsList from 'components/content/goods/GoodsList';
-import Scroll from 'components/common/scroll/Scroll';
-import BackTop from 'components/content/backTop/BackTop';
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
+import BackTop from "components/content/backTop/BackTop";
 
-import {getHomeMultidata,getHomeGoods} from "network/home";
-
-
+import { getHomeMultidata, getHomeGoods } from "network/home";
+import {debounce} from "common/utils";
 
 export default {
-  name:"Home",
+  name: "Home",
   components: {
     HomeSwiper,
     RecommendView,
@@ -42,96 +52,117 @@ export default {
     GoodsList,
     Scroll,
     BackTop,
-
   },
   data() {
     return {
-      banners:[],
-      recommends:[],
+      banners: [],
+      recommends: [],
       goods: {
-        'pop':{page: 0, list:[]},
-        'new':{page: 0, list:[]},
-        'sell':{page: 0, list:[]},
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
       },
-      currentType: 'pop'
-    }
-  },  
+      currentType: "pop",
+      isShowBackTop: false,
+      tabOffsetTop: 0,
+      isTabFixed: false
+    };
+  },
   computed: {
     showGoods() {
-      return this.goods[this.currentType].list
-    }
+      return this.goods[this.currentType].list;
+    },
   },
-  created(){
-    this.getHomeMultidata()
+  created() {
+    this.getHomeMultidata();
 
-    this.getHomeGoods('pop')
-    this.getHomeGoods('new')
-    this.getHomeGoods('sell')
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
-  methods:{
-    tabClick(index){
-      switch (index){
+  mounted() {
+    const refresh = debounce(this.$refs.scroll.refresh,200)
+    this.$bus.$on("itemImageLoad", () => {
+      refresh()
+    })
+  },
+  methods: {
+    tabClick(index) {
+      switch (index) {
         case 0:
-          this.currentType = 'pop'
-          break
+          this.currentType = "pop";
+          break;
         case 1:
-          this.currentType = 'new'
-          break
+          this.currentType = "new";
+          break;
         case 2:
-          this.currentType = 'sell'
-          break
-      }   
+          this.currentType = "sell";
+          break;
+      }
+      this.$refs.tabControl1.currentIndex = index;
+      this.$refs.tabControl2.currentIndex = index;
     },
     backClick() {
-      this.$refs.scroll.scrollTo(0,0);
+      this.$refs.scroll.scrollTo(0, 0);
     },
-    getHomeMultidata(){
-        getHomeMultidata().then(res => {
-          this.banners = res.data.banner.list;
-          this.recommends = res.data.recommend.list;
-        })
+    contentScroll(position) {
+      this.isShowBackTop = (-position.y) > 1000
+      this.isTabFixed = (-position.y) > this.tabOffsetTop
+    },
+    loadMore() {
+      this.getHomeGoods(this.currentType)
+
+    },
+    swiperImageLoad() {
+      this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop
+    },
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
     },
     getHomeGoods(type) {
-      const page = this.goods[type].page + 1
-      getHomeGoods(type,page).then(res => {
-        this.goods[type].list.push(...res.data.list)
-        this.goods[type].page += 1
-    })
-    }
-    
-     
-  }
-}
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+
+        this.$refs.scroll.finishPullUp()
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-  #home{
-    padding-top: 44px;
-    height: 100vh;
-    position: relative;
-  }
-  .home-nav{
-    background-color: var(--color-tint);
-    color: white;
-    position: fixed;
-    left: 0;
-    right: 0;
-    top: 0;
-    z-index: 9;
-  }
+#home {
+  
+  height: 100vh;
+  position: relative;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+  /* position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9; */
+}
 
-  .tab-control{
-    position: sticky;
-    top: 44px;
-    z-index: 9;
-  }
-  .content{
-    /* height: 300px; */
-    overflow: hidden;
-    position: absolute;
-    top: 44px;
-    bottom: 49px;
-    left: 0;
-    right: 0;
-  }
+.content {
+  /* height: 300px; */
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+
+.tab-control {
+  position: relative;
+  z-index: 9;
+}
 </style>
