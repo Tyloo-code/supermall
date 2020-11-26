4c68373187e361722bb11abcb2915a3560372495
bjfu_ltl <1002705465@qq.com>
2020-11-26 22:12:01 +0800

    移动端延时和懒加载

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
@@ -11,6 +12,13 @@ Vue.config.devtools = true;
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast)
+
+FastClick.attach(document.body)
+
+Vue.use(VueLazyLoad,{
+  loading: require('./assets/img/common/placeholder.png') //占位图
+})
+//还需将所有img标签的  src   改为 v-lazy
 
 new Vue({
   render: h => h(App),
